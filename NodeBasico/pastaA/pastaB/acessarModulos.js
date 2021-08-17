8d0f3ab13421082db432bb0048708220f8be61d7
Alex de Souza Marquezini <88939118+marqez@users.noreply.github.com>
2021-08-16 21:04:08 -0300

    Remember Basic 2

---
+++ NodeBasico/pastaA/pastaB/acessarModulos.js
@@ -0,0 +1,12 @@
+const moduloA = require('../../moduloA')
+const pastaC = require('./pastaC');
+
+console.log(moduloA.ola)
+
+console.log(pastaC.ola2)
+
+// const http = require('http')
+// http.createServer((req,res) => {
+//     res.write("Bom dia!")
+//     res.end()
+// }).listen(8888);
