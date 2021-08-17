fd958f9ff848711621034bca4766fe858595adda
Alex de Souza Marquezini <88939118+marqez@users.noreply.github.com>
2021-08-16 23:03:58 -0300

    Remember basics 3

---
+++ NodeBasico/funcionarios/funcionarios.js
@@ -0,0 +1,21 @@
+/*
+* Desafio da aula 153 
+*/
+const url = "http://files.cod3r.com.br/curso-js/funcionarios.json";
+const axios = require("axios");
+
+axios.get(url).then((response) => {
+  const funcionarios = response.data;
+
+  const menor = funcionarios
+    .filter((f) => {
+      if (f.genero === "F" && f.pais === "China") {
+        return true;
+      }
+      return false;
+    })
+    .reduce((min, cur) => {
+      return min.salario < cur.salario ? min : cur;
+    });
+  console.log(menor);
+});
