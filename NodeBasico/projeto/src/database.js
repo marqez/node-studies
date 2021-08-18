2a7d48fb9507ee6210d687969d5e31a3f24e7b4e
Alex de Souza Marquezini <88939118+marqez@users.noreply.github.com>
2021-08-18 20:40:35 -0300

    Api with Express 2

---
+++ NodeBasico/projeto/src/database.js
@@ -0,0 +1,24 @@
+const sequence = {
+  _id: 1,
+  get id() {
+    return this._id++;
+  },
+};
+
+const products = {};
+
+function saveProducts(product){
+    if(!product.id) product.id = sequence.id
+    products[product.id] = product
+    return product
+}
+
+function getProduct(id){
+    return products[id] || {}
+}
+
+function getProducts(){
+    return Object.values(products)
+}
+
+module.exports = {saveProducts, getProduct, getProducts}
--- NodeBasico/projeto/src/server.js
+++ NodeBasico/projeto/src/server.js
@@ -2,14 +2,25 @@ const port = 3003;
 
 const express = require("express");
 const app = express();
+const database = require("./database");
 
 app.get("/products", (req, res, next) => {
-  res.send({
-    nome: "notebook",
-    preco: 123.45,
+  res.send(database.getProducts());
+});
+
+app.get("/products/:id", (req, res, next) => {
+  res.send(database.getProduct(req.params.id));
+});
+
+app.post("/products", (req, res, next) => {
+  console.log(req);
+  const product = database.saveProducts({
+    name: req.body.name,
+    price: req.body.price,
   });
+  res.send(product);
 });
 
 app.listen(port, () => {
-    console.log(`Servidor executando na porta ${port}.`)
-})
+  console.log(`Servidor executando na porta ${port}.`);
+});
