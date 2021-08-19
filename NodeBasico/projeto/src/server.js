0e704e27a0061c28adf902e9f3964c549f286d37
Alex de Souza Marquezini <88939118+marqez@users.noreply.github.com>
2021-08-18 21:08:29 -0300

    Api with Express 3 + Insominia test file

--- NodeBasico/projeto/src/server.js
+++ NodeBasico/projeto/src/server.js
@@ -2,7 +2,14 @@ const port = 3003;
 
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 const database = require("./database");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 
 app.get("/products", (req, res, next) => {
   res.send(database.getProducts());
@@ -12,9 +19,23 @@ app.get("/products/:id", (req, res, next) => {
   res.send(database.getProduct(req.params.id));
 });
 
+app.delete("/products/:id", (req, res, next) => {
+  res.send(database.deleteProduct(req.params.id));
+});
+
 app.post("/products", (req, res, next) => {
   console.log(req);
   const product = database.saveProducts({
+    name: req.body.name,
+    price: req.body.price,
+  });
+  res.send(product);
+});
+
+app.put("/products/:id", (req, res, next) => {
+  console.log(req);
+  const product = database.saveProducts({
+    id: req.params.id,
     name: req.body.name,
     price: req.body.price,
   });
